e7e0f8f5ab8cafd25a5b76cec4cb40c633d2fd0d
Rushi <rushi@gmail.com>
2020-02-21 17:36:13 +0530

    question extraction succesfull

---
+++ OnlineExam2/src/com/lti/onlineexam/core/entities/Subject.java
@@ -0,0 +1,69 @@
+package com.lti.onlineexam.core.entities;
+
+import java.util.List;
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.Cascade;
+
+@Entity
+@Table(name="Subject")
+public class Subject {
+
+	@Id
+	@Column(name="SUBJECT_ID")
+	private int subjectId;
+	
+	@Column(name="SUBJECT_NAME")
+	private String subjectName;
+	
+	/*
+	 * @OneToMany(mappedBy = "subject", cascade = CascadeType.ALL) private
+	 * List<Question> question;
+	 * 
+	 * 
+	 * public List<Question> getQuestion() { return question; }
+	 * 
+	 * public void setQuestion(List<Question> question) { this.question = question;
+	 * }
+	 */
+
+	public Subject() {
+		super();		
+	}
+
+	public Subject(int subjectId, String subjectName) {
+		super();
+		this.subjectId = subjectId;
+		this.subjectName = subjectName;
+	}
+
+	public int getSubjectId() {
+		return subjectId;
+	}
+
+	public void setSubjectId(int subjectId) {
+		this.subjectId = subjectId;
+	}
+
+	public String getSubjectName() {
+		return subjectName;
+	}
+
+	public void setSubjectName(String subjectName) {
+		this.subjectName = subjectName;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject [subjectId=" + subjectId + ", subjectName=" + subjectName + "]";
+	}
+}
